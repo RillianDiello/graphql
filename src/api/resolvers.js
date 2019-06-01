aae0440536c4a04951c11320f572b0ca0387fd18
RillianDiello <rilliansf@gmail.com>
2019-06-01 14:44:09 -0400

    Usando o GraphQl

---
+++ src/api/resolvers.js
@@ -0,0 +1,30 @@
+/*Manipulam os grafos de dados
+Etiquta Graphql
+query = apenas obtem dados do servidor
+mutations = modificam dados no servidor
+*/
+
+const db = require('../config/database')
+
+module.exports = {
+    Query: {
+        async getUser(_, { id }) {
+            return await db('users').where({ id }).first()
+        },
+        async getUsers() {
+            return await db('users')
+        }
+    },
+    Mutation: {
+        async createUser (_, { input }) {
+            const result = await db('users').insert({
+                name: input.name,
+                email: input.email,
+                password: input.password
+            })
+
+            const id = result[0]
+            return await db('users').where({ id }).first()
+        }
+    }
+}
